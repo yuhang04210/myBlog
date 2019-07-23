e646190c2f295ece756030ce6a117c7de4dabd1b
yuhang <377849759@qq.com>
2019-07-23 16:32:01 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ const sidebars = filehelper.getSidebar();
 module.exports = {
     base: "/myBlog/",
     title: "my blog",
-    description: "我的个人网站",
+    description: "",
     head: [
         // 注入到当前页面的 HTML <head> 中的标签
         ["link", { rel: "icon", href: "/logo.jpg" }] // 增加一个自定义的 favicon(网页标签的图标)
